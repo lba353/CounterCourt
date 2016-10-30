38a13a23b89c412fb4f0cb2082c2787730ecbec8
Ludvig Basmajyan <Lba353@users.noreply.github.com>
2016-10-30 11:23:26 -0700

    Milestone 6
    Changed the Reset button to a Game Over button. Game Over button, when
    clicked, now has dialog and will show who won the game.

--- app/src/main/java/com/example/student/courtcounter/MainActivity.java
+++ app/src/main/java/com/example/student/courtcounter/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.student.courtcounter;
 
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -11,6 +13,7 @@ public class MainActivity extends AppCompatActivity {
     private TextView goal;
     private int s = 0;
     private int g = 0;
+    private AlertDialog gameOver;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +25,8 @@ public class MainActivity extends AppCompatActivity {
 
         goal = (TextView)findViewById(R.id.goals);
         goal.setText(String.valueOf(g));
+
+        gameOver = new AlertDialog.Builder(MainActivity.this).create();
     }
 
     public void click1(View view) {
@@ -35,10 +40,42 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void reset(View view){
+        alert();
+
         g = 0;
         s = 0;
 
         goal.setText(String.valueOf(g));
         score.setText(String.valueOf(s));
+    }
+
+    public void alert() {
+        //Lines 54-64 are if statements that state who won the game and show the message in the alert box.
+        if(s > g) {
+            gameOver.setMessage("Team Red has won the game!");
+        }
+
+        if(s < g) {
+            gameOver.setMessage("Team Blue has won the game!");
+        }
+
+        if(s == g) {
+            gameOver.setMessage("Draw! Nobody Wins!");
+        }
+
+
+        //Sets the title for the alert box.
+        gameOver.setTitle("Game Over!");
+
+        //The text box has a button that says, "OK" and will close the alert when pressed.
+        gameOver.setButton(AlertDialog.BUTTON_NEUTRAL, "OK",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                });
+
+        //Shows the alert
+        gameOver.show();
     }
 }
